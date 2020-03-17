51c92229b3f068e3ed2befdf18a0da0defdc8c5e
Laser <61454394+LaserDragon84@users.noreply.github.com>
2020-03-17 16:51:09 +0800

    Fixed errors

--- python/practice.py
+++ python/practice.py
@@ -3,9 +3,9 @@ import json
 import datetime
 
 # Init Variables
-welcomeMessage = "--- Weather Pyvien ---"
+welcomeMessage = "--- Eric's Weather API ---"
 date = datetime.datetime.now()
-date = date.strftime("%y/%-m/%-d")
+date = date.strftime("%Y/%-m/%d")
 
 # Welcome to the Weather API
 print(welcomeMessage)
@@ -20,10 +20,9 @@ data = json.loads(response.read())
 woeid = data[0]['woeid']
 
 # Get Weather
-link = 'https://www.metaweather.com/api/???/' + int(woeid) + '/' + date
+link = 'https://www.metaweather.com/api/location/' + str(woeid) + '/' + date
 response = urlopen(link)
-data = json.loads(response.read()
-
+data = json.loads(response.read())
 # Print it to the screen
 print("Type of weather: " + data[0]["weather_state_name"])
 print("Minimum Temperature: " + str(round(data[0]["min_temp"])))
